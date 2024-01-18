6555fa77f7461deb95145a6314bdec22e06375ed
monalucy <monicagliga502@yahoo.co.uk>
2024-01-18 03:01:40 +0000

    Changes made

---
+++ Basic_HTML_and_JavaScript/Basic_JavaScript_1.js
@@ -0,0 +1,2 @@
+//JavaScript code for displaying an alert
+alert ('Hello, this is a JavaScript alert!');
---
+++ Basic_HTML_and_JavaScript/Basic_JavaScript_2.js
@@ -0,0 +1,6 @@
+// Basic_JavaScript_1.js
+
+function displayMessage() {
+    var element = document.getElementById("output");
+    element.innerHTML = "Hello, World!";
+}
